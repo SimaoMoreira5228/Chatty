4175b881a5740210fe9527da7ad10da0cd549c89
Dewn5228 <105013133+SimaoMoreira5228@users.noreply.github.com>
2023-10-27 21:33:44 +0100

    main react files

---
+++ src/App.tsx
@@ -0,0 +1,212 @@
+import { Settings, Trash } from "lucide-react";
+import { Message } from "./components/customised/Message";
+import { Button } from "@/components/ui/button";
+import { ConfigModal } from "./components/customised/ConfigModal";
+import { FFEmotes, type WebsocketMessageData } from "@/lib/types";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import { Spinner } from "./svgs/Spinner";
+
+export const App = () => {
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
+  const [isConfigModalOpen, setConfigModalOpen] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [betterEmotes, setBetterEmotes] = useState([]);
+  const [frankerFaceZEmotes, setFrankerFaceZEmotes] = useState<any[]>([]);
+  const [defaultValues, setDefaultValues] = useState({
+    twitchUser: "",
+    youtubeVideoId: "",
+    youtubeApiKey: "",
+    kickUser: "",
+  });
+  const [messages, setMessages] = useState<Map<string, WebsocketMessageData>>(
+    new Map()
+  );
+
+  useEffect(() => {
+    const client = new WebSocket("ws://localhost:1348");
+
+    client.addEventListener("message", (event) => {
+      const data = JSON.parse(event.data);
+      const {
+        id,
+        name,
+        provider,
+        isSub,
+        isMod,
+        isOwner,
+        badges,
+        badgesRaw,
+        emotes,
+        emotesRaw,
+        message,
+        channelName,
+      } = data;
+      setMessages((messages) => {
+        messages.set(id, {
+          id,
+          name,
+          provider,
+          isSub,
+          isMod,
+          isOwner,
+          badges,
+          badgesRaw,
+          emotes,
+          emotesRaw,
+          message,
+          channelName,
+        });
+        return new Map(messages);
+      });
+    });
+
+    client.addEventListener("error", (error) => {
+      console.error("WebSocket error: ", error);
+    });
+
+    client.addEventListener("close", () => {
+      console.log("WebSocket connection closed");
+    });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://api.betterttv.net/3/cached/emotes/global")
+      .then((res) => {
+        setBetterEmotes(res.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get("https://api.frankerfacez.com/v1/set/global").then((res) => {
+      const sets = res.data.sets as Record<string, { emoticons: FFEmotes[] }>;
+      for (const set of Object.values(sets)) {
+        setFrankerFaceZEmotes((emotes) => [...emotes, ...set.emoticons]);
+      }
+    });
+  }, []);
+
+  const openConfigModal = async () => {
+    try {
+      setLoading(true);
+      const data = axios
+        .get("http://localhost:1349/api/get-config")
+        .then((res) => res.data);
+      const { twitchUser, youtubeVideoId, youtubeApiKey, kickUser } =
+        await data;
+      setDefaultValues({
+        twitchUser,
+        youtubeVideoId,
+        youtubeApiKey,
+        kickUser,
+      });
+      setConfigModalOpen(true);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const closeConfigModal = () => {
+    setConfigModalOpen(false);
+  };
+
+  const saveConfig = (
+    twitchUser: string,
+    youtubeVideoId: string,
+    youtubeApiKey: string,
+    kickUser: string
+  ) => {
+    try {
+      setLoading(true);
+      const configData = {
+        twitchUser,
+        youtubeVideoId,
+        youtubeApiKey,
+        kickUser,
+      };
+      axios.post("http://localhost:1349/api/update-config", configData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setConfigModalOpen(false);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const trashMessages = () => {
+    setMessages(new Map());
+  };
+
+  useEffect(() => {
+    const container = messagesContainerRef.current;
+    if (container) {
+      container.scrollTop = container.scrollHeight;
+    }
+  }, [messages]);
+
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <Button
+        variant="outline"
+        size="icon"
+        className="top-2 right-2 absolute"
+        onClick={openConfigModal}
+      >
+        <Settings className="h-4 w-4" />
+      </Button>
+      <Button
+        variant="outline"
+        size="icon"
+        className="top-2 right-14 absolute"
+        onClick={trashMessages}
+      >
+        <Trash className="h-4 w-4" />
+      </Button>
+
+      <div
+        ref={messagesContainerRef}
+        className="flex flex-col w-[85%] h-[80%] rounded-2xl p-4 overflow-y-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-primary scrollbar-track-background"
+      >
+        {Array.from(messages.values()).map((message, index) => (
+          <Message
+            key={index}
+            name={message.name}
+            provider={message.provider}
+            isSub={message.isSub}
+            isMod={message.isMod}
+            badges={message.badges}
+            badgesRaw={message.badgesRaw}
+            emotes={message.emotes}
+            emotesRaw={message.emotesRaw}
+            channelName={message.channelName}
+            message={message.message}
+            betterEmotes={betterEmotes}
+            frankerFaceZEmotes={frankerFaceZEmotes}
+            className="mb-2"
+          />
+        ))}
+      </div>
+      {isConfigModalOpen && (
+        <ConfigModal
+          closeConfigModal={closeConfigModal}
+          saveConfig={saveConfig}
+          defaultValues={defaultValues}
+        />
+      )}
+      {isLoading && (
+        <div className="fixed z-50 flex justify-center items-center w-screen h-screen bg-gray-600 bg-opacity-30">
+          <Spinner className="w-4 h-4" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
